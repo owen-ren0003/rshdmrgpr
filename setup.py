7ae28aeab9bfe967d166b11b02a5db7d2ec78b84
Owen Ren <owen.ren@purefacts.com>
2020-09-03 22:10:11 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(# Needed to silence warnings (and to be a worthwhile package)
     name='rshdmrgpr',
-    url='git@github.com:owen-ren0003/rshdmrgpr.git',
+    url='git@github.com/owen-ren0003/rshdmrgpr.git',
     author='Owen Ren',
     author_email='owen.z.ren@1234@gmail.com',
     # Needed to actually package something
@@ -14,6 +14,7 @@ setup(# Needed to silence warnings (and to be a worthwhile package)
     # The license can be anything you like
     license='MIT',
     description='RS-HDMR-GPR',
+    zip_safe=False
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
