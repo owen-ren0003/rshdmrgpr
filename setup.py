62306796c1469d2c5d47acfd0b3937c78801e15e
Owen Ren <owen.ren@purefacts.com>
2020-09-03 22:13:20 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(# Needed to silence warnings (and to be a worthwhile package)
     # *strongly* suggested for sharing
     version='0.0.1',
     # The license can be anything you like
-    license='MIT',
+    license='unlicense',
     description='RS-HDMR-GPR',
     zip_safe=False
     # We will also need a readme eventually (there will be a warning)
