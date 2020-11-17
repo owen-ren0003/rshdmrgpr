a4b70704ab32f1d148f9149595ca9cd5e800724e
Owen Ren <owen.ren@purefacts.com>
2020-11-17 12:28:26 -0800

    Updated Code with LICENSE and readme.md

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Introduction
+
+This package contains the code that supplements the paper 
+
+## Requirements
+The following packages are required in order to use this package.
+
+[matplotlib>=3.1.3](https://matplotlib.org/)  
+[numpy>=1.18.1](https://numpy.org/)    
+[pandas>=1.1.0](https://pandas.pydata.org/)  
+[scikit-learn>=0.22.1](https://scikit-learn.org/stable/)  
+
+# Installation
+
+To install and use this package, open any python terminal and install using the following code:
+
+```
+pip install git+https://github.com/owen-ren0003/rshdmrgpr.git
+```
+
+```
+import rshdmrgpr
+```
--- rshdmrgpr/rs_hdmr_gpr.py
+++ rshdmrgpr/rs_hdmr_gpr.py
@@ -13,6 +13,7 @@ import time
 def correlation_plot(true, prediction, y_err=None, save_fig=None, dpi=None):
     """
     Graphs the correlation between true values and predicted values and saves the image.
+
     :param true: list like
         The true values.
     :param prediction: list like
@@ -44,6 +45,7 @@ def correlation_plot(true, prediction, y_err=None, save_fig=None, dpi=None):
 def kernel_matrices(order, dim, length_scale):
     """
     Helper function used to create the RBF kernels and the matrix input for HDMR.
+
     :param order: int
         The order of HDMR to use.
     :param dim: int
@@ -69,7 +71,8 @@ def kernel_matrices(order, dim, length_scale):
 class RSHDMRGPR:
     def __init__(self, num_models, matrices, kernels):
         """
-        Initializes an instance of GPRHDMR object.
+        Initializes an instance of RS-HDMR-GPR object.
+
         :param num_models: int
             number of Guassian models to be used.
         :param matrices: list of 2D numpy.Arrays
@@ -100,6 +103,7 @@ class RSHDMRGPR:
     def train(self, data, label='out', alphas=1e-7, cycles=50, scale_down=(0.1, 1), report_rmse=False):
         """
         Trains the 1st order HDMR.
+
         :param data: DataFrame
             Contains both the the features and the label column
         :param label: str
@@ -113,12 +117,14 @@ class RSHDMRGPR:
             fraction of scale down, and step size represents a rate at which this fraction increases.
         :param report_rmse: bool
             Saves the rmse on training over cycles.
-        :return:
-            list of training labels
-                list of labels used to carry out all intermediate trainings.
-            list of models
-                list of the trained models in order.
+        :return: Returns 1) if report_rmse = True, 2) otherwise.
+            1) self, pandas DataFrame
+                The first argument is the trained instance of self. The second contains the RMSE of predicted
+                values (vs actual) on the training set for each cycle.
+            2) self
+                The trained instance of self.
         """
+
         # Checks if the model has been trained already. Only trains once.
         if self.__is_trained:
             raise RuntimeError('Model has already been trained')
@@ -152,6 +158,7 @@ class RSHDMRGPR:
         y_i = [y_train / self.__num_models] * self.__num_models
 
         if report_rmse:
+            # used to display the RMSE per cycle on the training set.
             df_rmse = pd.DataFrame(columns=['cycle_no', 'rmse'])
             df_rmse['cycle_no'] = np.arange(1, cycles + 1)
 
@@ -173,25 +180,28 @@ class RSHDMRGPR:
                 y_i[i] = gpr.predict(df) * min(start + (1 - start) * step * (c + 1) / cycles, 1)
 
             if report_rmse:
+                # Calculates and record the RMSE on the training set for each cycle
                 predicted = self.predict(x_train)
                 df_rmse.loc[df_rmse['cycle_no'] == c + 1, 'rmse'] = math.sqrt(mean_squared_error(predicted, y_train))
 
         print('Training completed.')
-
         if report_rmse:
             return self, df_rmse
         return self
 
     def predict(self, test_data, return_std=False):
         """
-        Predicts the label using features from test_data
+        Predicts the label using features from test_data.
+
         :param test_data: DataFrame
             Must have the same number of input features as the data the model was trained on.
         :param return_std: bool
             Returns the standard deviation of the predictions if True.
-        :return: numpy.ndarray return_std=False, (np.ndarray, np.ndarray) if return_std=True
-            1) The predicted probabilities.
-            2) The predicted probabilities and error.
+        :return: 1) return_std=False, 2) otherwise.
+            1) numpy.ndarray, numpy.ndarray
+                The predicted values and the square-root of the sum of variances.
+            2) numpy.ndarray
+                The predicted values
         """
         if not self.__is_trained:
             raise RuntimeError('This model has not been trained. Train the model before telling it to predict.')
@@ -214,8 +224,10 @@ class RSHDMRGPR:
 
     def get_models(self):
         """
-        Returns the trained models
-        :return: The trained models
+        Returns the trained component function models.
+
+        :return: list of GaussianProcessRegressor
+            The trained hdmr component functions
         """
         if not self.__is_trained:
             raise RuntimeError("Model must be trained first before it can be returned.")
@@ -226,18 +238,21 @@ class RSHDMRGPR:
 class FirstOrderHDMRImpute:
     def __init__(self, models, division=1000):
         """
-        Initializes the class by vectorizing the 1D HDMR component functions and creating a dictionary of output values
-        with [division] subdivisions.
-        :param models: list of trained sklearn.gaussian_process.GaussianProcessRegressor models.
-            Contains the hdmr component functions of first order. Must be of first order (1 input and 1 output).
+        Initializes the class by vectorizing the 1D HDMR component functions and creating a lookup DataFrame whose first
+        column are the endpoints of [division] subdivisions of the interval [0, 1] and the remaining columns are the
+        y_i := f_i(column_1) where f_i is the i-th HDMR component function.
+
+        :param models: list of GaussianProcessRegressor
+            Contains the trained HDMR component functions of first order. Must be of first order (1 input and 1 output).
         :param division: int
-            The number of divisions in the lookup table
+            The number of divisions in the lookup table.
         """
         self.__models = models
         self.__num_models = len(self.__models)
         self.__table_yi = pd.DataFrame(np.linspace(0, 1, division + 1), columns=['input'])
+        self.__gap = 2 / division  # Used for imputation to set minimum distance threshold.
 
-        # Computing the lookup table for HDMR values
+        # Computes the lookup table for the first component function values
         def model_func(x, idx):
             if pd.isna(x):
                 return np.nan
@@ -249,56 +264,72 @@ class FirstOrderHDMRImpute:
         for i in range(len(models)):
             self.__table_yi['y_' + str(i)] = self.__model_func(self.__table_yi['input'], i)
 
-    def get_yi(self, df_na, label='out'):
+    def get_table(self):
         """
-        Modifies the DataFrame to contain the outputs of the first hdmr component functions.
+        Returns the lookup table for the component functions.
 
-        Adds n additional columns to df_na, where n is the number of feature columns in df_na. The column
+        :return: panda DataFrame
+            Returns the lookup table for the HDMR component functions.
+        """
+        return self.__table_yi
+
+    def get_yi(self, df_na):
+        """
+        Modifies the DataFrame to contain the outputs of the first HDMR component functions.
+
+        Adds n additional columns to df_na, where n is the number of feature columns in df_na. The label column
+        must be the last column or the function won't work.
 
         :param df_na: pandas DataFrame
             The dataframe to impute. Must contain the 'output' column and must be the last column.
-        :param label: str
-            The column representing the label.
         :return: list
             Indices of the rows corresponding to nan columns.
         """
 
         n = df_na.shape[1] - 1
+
         # Creates the extra columns to the right of the last column of df_na that contains the outputs of the features
         # from the trained component functions. For instance y_0 will be the output of the 0th indexed column of df_na.
         for i in range(self.__num_models):
             df_na['y_' + str(i)] = self.__model_func(df_na.iloc[:, i], i)
 
-        nan_rows = df_na.loc[df_na.isnull().any(axis=1)]  # Selects the rows with missing values.
-        nan_rows_idx = nan_rows.index  # Selects the index of those rows with missing values
-        null_rows = nan_rows.isnull().idxmax(axis=1)  # Selects the corresponding column name to the missing value entry
+        nan_rows = df_na.loc[df_na.isnull().any(axis=1)]  # Extracts the rows with missing values.
+        nan_rows_idx = nan_rows.index  # Extracts the index of those rows with missing values
+        nan_cols = nan_rows.isnull().idxmax(axis=1)  # Selects the corresponding column name to the missing value entry
 
-        col_to_ord = {}
+        # column to index dictionary for lookup
+        col_to_idx = {}
         for i in range(n):
-            col_to_ord[df_na.columns[i]] = i
+            col_to_idx[df_na.columns[i]] = i
 
-        #
-        for i in range(null_rows.shape[0]):
-            idx = null_rows.index[i]  # index of the ith null row occurrence
-            col_no = col_to_ord[null_rows.iloc[i]]  # the column number
-            val = df_na[label].loc[idx] - df_na.loc[idx, :][n + 1:].sum(axis=0)
+        # Computes y_i corresponding to the missing value column.
+        for i in range(nan_cols.shape[0]):
+            idx = nan_rows_idx[i]  # index of the ith null row occurrence
+            col_no = col_to_idx[nan_cols.iloc[i]]  # the column number
+            val = df_na.loc[idx, :][n] - df_na.loc[idx, :][n + 1:].sum(axis=0)
             df_na.loc[idx, :][n + 1 + col_no] = val
 
         return nan_rows_idx
 
-    def impute(self, df_na, impute_candidates=False):
+    def impute(self, df_na, get_candidates=False, threshold=0.001):
         """
         This function imputes the missing values given the input.
 
         :param df_na: pandas DataFrame
             The DataFrame to impute, should contain the columns corresponding to 1D hdmr outputs.
-        :param impute_candidates: bool
+        :param get_candidates: bool
             Option to return the imputed candidates or not.
-        :return: pandas DataFrame
-            The imputed DataFrame.
+        :param threshold: float
+            Set the threshold distance for selecting from look-up table.
+        :return: 1) if get_candidates=True, 2) otherwise
+            1) (pandas DataFrame, pandas Index, pandas Series, list of float)
+                Contains the imputed data, the index of rows with null entries,
+            2) pandas DataFrame
+                Contains the imputed data.
         """
         start = time.time()
-        nan_rows = df_na.loc[df_na.isnull().any(axis=1)]  # Selects the rows with missing values.
+        # Selects the rows with missing values.
+        nan_rows = df_na.loc[df_na.isnull().any(axis=1)].copy()
         # Selects the corresponding column name to the missing value entry.
         null_entry = nan_rows.isnull().idxmax(axis=1)
 
@@ -310,50 +341,59 @@ class FirstOrderHDMRImpute:
         def get_inputs(num, col, df=self.__table_yi):
             """
             Determines all inputs that correspond to an interval containing num.
+
             :param num: int
                 The number to find the input value for.
             :param col: int
                 The column corresponding to the lookup table.
             :param df: pandas DataFrame
                 The lookup table.
-            :return: list
+            :return: list of float
                 List of possible candidates.
             """
+            # Obtains the candidates within self.__gap distance
             min_yi = df.iloc[:, col].min()
-            min_yi_idx = df.loc[df.iloc[:, col] == min_yi, 'input'].min()
+            min_yi_idx = df.loc[abs(df.iloc[:, col] - min_yi) <= self.__gap, 'input'].tolist()
             max_yi = df.iloc[:, col].max()
-            max_yi_idx = df.loc[df.iloc[:, col] == max_yi, 'input'].max()
+            max_yi_idx = df.loc[abs(df.iloc[:, col] - max_yi) <= self.__gap, 'input'].tolist()
 
-            ret_idxs = []
-            for i in range(df.shape[0] - 1):
-                left = df.iloc[i, col]
-                right = df.iloc[i + 1, col]
+            ret_inputs = []
+            for i in range(df.shape[0]):
+                val = df.iloc[i, col]
+                if i < df.shape[0] - 1:
+                    left = df.iloc[i, col]
+                    right = df.iloc[i + 1, col]
+                    if left <= num <= right:
+                        ret_inputs.append(df.iloc[i, 0])
+                    elif right <= num < left:
+                        ret_inputs.append(df.iloc[i + 1, 0])
+                    elif abs(val - num) < threshold:
+                        ret_inputs.append(df.iloc[i, 0])
+                elif abs(val - num) < threshold:
+                    ret_inputs.append(df.iloc[i, 0])
 
-                if left <= num < right:
-                    ret_idxs.append(df.iloc[i, 0])
-                elif right <= num < left:
-                    ret_idxs.append(df.iloc[i + 1, 0])
+            if min_yi >= num:
+                ret_inputs.extend(min_yi_idx)
+            elif max_yi <= num:
+                ret_inputs.extend(max_yi_idx)
 
-            if len(ret_idxs) == 0:
-                if min_yi >= num:
-                    ret_idxs.append(min_yi_idx)
-                elif max_yi <= num:
-                    ret_idxs.append(max_yi_idx)
+            return ret_inputs
 
-            return ret_idxs
-
-        if impute_candidates:
+        # Will return all the possible imputation candidates if this option is set to True.
+        if get_candidates:
             candidates = []
 
         for i in range(nan_rows.shape[0]):
             col_no = col_to_ord[null_entry.iloc[i]]  # changes the column name to column index.
             val = get_inputs(nan_rows.iloc[i, col_no + self.__num_models + 1], col_no + 1)
             nan_rows.iloc[i, col_no] = val[0]
-            if impute_candidates:
+
+            if get_candidates:
                 candidates.append(val)
 
         df_na.loc[df_na.isnull().any(axis=1)] = nan_rows
         print(f'Function execution time took {time.time() - start} seconds.')
-        if impute_candidates:
-            return df_na, nan_rows.index, candidates
+
+        if get_candidates:
+            return df_na, nan_rows.index, null_entry, candidates
         return df_na
